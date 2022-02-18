6315ae4cd2e2aaed7307badb5d2fd6d567846522
Ariel <arielox.ag@gmail.com>
2022-02-17 22:29:11 -0300

    listo back parte 1

--- routes/posts.js
+++ routes/posts.js
@@ -16,18 +16,87 @@ router.post('/', async (req, res) => {
 });
 
 //update a post
-router.put('/:id', async (req, res) => {});
+router.put('/:id', async (req, res) => {
+   try {
+      const post = await Post.findById(req.params.id);
+
+      if (post.userId === req.body.userId) {
+         await post.updateOne({ $set: req.body }); // updatea todos los inpus q esten en el body
+         res.status(200).json('The post has been updated successfully');
+      } else {
+         res.status(403).json('you can update only your post');
+      }
+   } catch (error) {
+      res.status(500).json(err);
+   }
+});
 
 //delete a post
-router.delete('/:id', async (req, res) => {});
+router.delete('/:id', async (req, res) => {
+   try {
+      const post = await Post.findById(req.params.id);
+
+      if (post.userId === req.body.userId) {
+         await post.deleteOne();
+
+         res.status(200).json('the post has been deleted');
+      } else {
+         res.status(403).json('you can delete only your post');
+      }
+   } catch (error) {
+      res.status(500).json(err);
+   }
+});
 
 //like / dislike a post
-router.put('/:id/like', async (req, res) => {});
+router.put('/:id/like', async (req, res) => {
+   try {
+      const post = await Post.findById(req.params.id);
+
+      if (!post.likes.includes(req.body.userId)) {
+         // en este post en "likes" mete mi "id"
+         await post.updateOne({ $push: { likes: req.body.userId } });
+
+         res.status(200).json('The post has been liked');
+      } else {
+         await post.updateOne({ $pull: { likes: req.body.userId } });
+
+         res.status(200).json('The post has been disliked');
+      }
+   } catch (error) {
+      res.status(500).json(err);
+   }
+});
 
 //get a post
-router.get('/:id', async (req, res) => {});
+router.get('/:id', async (req, res) => {
+   try {
+      const post = await Post.findById(req.params.id);
+
+      res.status(200).json(post);
+   } catch (error) {
+      res.status(500).json(error);
+   }
+});
 
 //get timeline posts
-router.get('/timeline/all', async (req, res) => {});
+router.get('/timeline/all', async (req, res) => {
+   try {
+      const currentUser = await User.findById(req.body.userId);
+      const userPosts = await Post.find({ userId: currentUser._id });
+
+      // devuelve todos los posts de los friends
+      // el "Promise.all" es xq uso "map" dentro del await
+      const friendsPosts = await Promise.all(
+         currentUser.followings.map(friendId => {
+            return Post.find({ userId: friendId });
+         })
+      );
+
+      res.json(userPosts.concat(...friendsPosts));
+   } catch (error) {
+      res.status(500).json(err);
+   }
+});
 
 module.exports = router;
