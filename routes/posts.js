7c8d3535159704973e04626a17dda15504d296b7
Ariel <arielox.ag@gmail.com>
2022-02-21 23:27:39 -0300

    acabando el upload image

--- routes/posts.js
+++ routes/posts.js
@@ -48,7 +48,7 @@ router.delete('/:id', async (req, res) => {
    }
 });
 
-//like / dislike a post
+// like / dislike a post
 router.put('/:id/like', async (req, res) => {
    try {
       const post = await Post.findById(req.params.id);
