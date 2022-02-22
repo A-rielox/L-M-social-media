bfbba80df5e7b58a7735a3136fb6714585ee19a1
Ariel <arielox.ag@gmail.com>
2022-02-22 13:16:27 -0300

    listo el upload

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ const storage = multer.diskStorage({
       cb(null, 'public/images');
    },
    filename: (req, file, cb) => {
-      // cb(null, req.body.name);
-      cb(null, file.originalname);
+      cb(null, req.body.name);
+      // cb(null, file.originalname);
    },
 });
 
