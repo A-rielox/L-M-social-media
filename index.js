53b25dd1df4090dad561e230f42ba080f97123a3
Ariel <arielox.ag@gmail.com>
2022-02-22 12:28:02 -0300

    file upload a server

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const helmet = require('helmet');
 const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
 // ===== DB
 const connectDB = require('./db/connect');
+
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 // ===== ROUTERS
 const userRoute = require('./routes/users');
@@ -18,6 +22,26 @@ const postRoute = require('./routes/posts');
 app.use(express.json()); // el body-parser
 app.use(helmet());
 app.use(morgan('tiny'));
+
+//===== MULTER pa subir archivos
+const storage = multer.diskStorage({
+   destination: (req, file, cb) => {
+      cb(null, 'public/images');
+   },
+   filename: (req, file, cb) => {
+      // cb(null, req.body.name);
+      cb(null, file.originalname);
+   },
+});
+
+const upload = multer({ storage: storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+   try {
+      return res.status(200).json('File uploded successfully');
+   } catch (error) {
+      console.error(error);
+   }
+});
 
 // @@@@@@@@@@@@@@@@@@@@ RUTAS
 app.use('/api/users', userRoute);
